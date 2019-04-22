e5f01b4c6e7f5637adfb985cefb2321c23f2b6fb
Charalampos <44839448+CharalamposSam@users.noreply.github.com>
2019-04-22 17:45:40 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,36 +1,51 @@
-const slider = document.querySelector( ".slider" )
-    let isDown = false
-    let startX
-    let scrollLeft
-
-    slider.addEventListener( "mousedown", msDown )
-    slider.addEventListener( "touchstart", msDown )
-    slider.addEventListener( "mouseleave", msLeave )
-    slider.addEventListener( "touchend", msLeave )
-    slider.addEventListener( "mouseup", msUp )
-    slider.addEventListener( "touchend", msUp )
-    slider.addEventListener( "mousemove", msMove )
-    slider.addEventListener( "touchmove", msMove )
-
-    function msDown( e ) {
-        e.preventDefault()
-        isDown = true
-        slider.classList.add( "active" )
-        startX = e.pageX - slider.offsetLeft
-        scrollLeft = slider.scrollLeft
-    }
-    function msLeave( ) {
-        isDown = false
-        slider.classList.remove( "active" )
-    }
-    function msUp( ) {
-        isDown = false
-        slider.classList.remove( "active" )
-    }
-    function msMove( e ) {
-        if ( !isDown ) return
-        e.preventDefault()
-        const x = e.pageX - slider.offsetLeft
-        const walk = (x - startX) * 5
-        slider.scrollLeft = scrollLeft - walk
-    }
+const slider = document.querySelector(".slider");
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+    // for desktop
+    slider.addEventListener("mousedown", e => {
+    e.preventDefault();
+    isDown = true;
+    slider.classList.add("active");
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+    });
+    // for mobile
+    slider.addEventListener("touchstart", e => {
+    e.preventDefault();
+    isDown = true;
+    slider.classList.add("active");
+    startX = e.touches[0].pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+    });
+    // for desktop
+    slider.addEventListener("mouseleave", () => {
+    isDown = false;
+    slider.classList.remove("active");
+    });
+    // for mobile
+    slider.addEventListener("touchend", () => {
+    isDown = false;
+    slider.classList.remove("active");
+    });
+    // for desktop
+    slider.addEventListener("mouseup", () => {
+    isDown = false;
+    slider.classList.remove("active");
+    });
+    // for desktop
+    slider.addEventListener("mousemove", e => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 5;
+    slider.scrollLeft = scrollLeft - walk;
+    });
+    // for mobile
+    slider.addEventListener("touchmove", e => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.touches[0].pageX - slider.offsetLeft;
+    const walk = (x - startX) * 2;
+    slider.scrollLeft = scrollLeft - walk;
+    });
